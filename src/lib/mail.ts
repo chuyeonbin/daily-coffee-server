b12942947ea5a27e727e638e24b9f6e42dc1c91c
ChuYeonBin <cndusqls98@gmail.com>
2023-07-07 14:14:48 +0900

    feat: nodemailer를 이용한 메일 전송 구현

---
+++ src/lib/mail.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  service: process.env.EMAIL_SERVICE,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  secure: true,
+});
+
+export default function sendEmail(email: string) {
+  return transporter.sendMail({
+    from: `"verify@dailycoffee.io" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: 'daily coffee 로그인',
+    text: '노드 패키지 nodemailer를 이용해 보낸 이메일임',
+    html: '<b>Hello world?</b>',
+  });
+}
---
+++ src/routes/auth.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import getConnection from '../database';
+import sendEmail from '../lib/mail';
+
+const router = express.Router();
+
+router.post('/sendemail', async (req, res) => {
+  const email: string = req.body.email;
+
+  try {
+    const info = await sendEmail(email);
+
+    console.log(info);
+  } catch (err) {
+    res.status(400).json('fail');
+    throw err;
+  }
+
+  res.status(200).json('ok');
+});
+
+export default router;
