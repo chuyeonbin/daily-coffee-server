d98f0fe393b8607f928fe5c50241bf8965106e57
ChuYeonBin <cndusqls98@gmail.com>
2023-07-10 23:10:01 +0900

    fix: mail 관련 함수 class로 바꿈

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,23 +1,32 @@
 import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  service: process.env.EMAIL_SERVICE,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  secure: true,
-});
+class Email {
+  private transporter: nodemailer.Transporter<SMTPTransport.SentMessageInfo>;
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      service: process.env.EMAIL_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      secure: true,
+    });
+  }
 
-export default function sendEmail(email: string) {
-  return transporter.sendMail({
-    from: `"verify@daily-coffee.io" ${process.env.EMAIL_USER}`,
-    to: email,
-    subject: 'daily coffee 로그인',
-    text: '노드 패키지 nodemailer를 이용해 보낸 이메일임',
-    html: '<b>Hello world?</b>',
-  });
+  sendEmail(mailOptions: Mail.Options) {
+    return this.transporter.sendMail({ ...mailOptions });
+  }
+
+  closeMail() {
+    this.transporter.close();
+  }
 }
+
+const mailer = new Email();
+
+export default mailer;
