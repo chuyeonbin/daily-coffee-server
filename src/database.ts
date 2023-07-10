d049e7ffe7af8e8b56d4a2d7fc86a48e697c1c6c
ChuYeonBin <cndusqls98@gmail.com>
2023-07-10 16:48:20 +0900

    fix: pool promise pool로 수정

--- src/database.ts
+++ src/database.ts
@@ -7,20 +7,11 @@ const dbConfig: PoolOptions = {
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT as unknown as number,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWARD,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MTSQL_DATABASE,
   connectionLimit: 30,
 };
 
-const pool = createPool(dbConfig);
+const pool = createPool(dbConfig).promise();
 
-const getConnection = (callback: (connection: PoolConnection) => void) => {
-  pool.getConnection((err, connection) => {
-    if (!err) {
-      callback(connection);
-    } else {
-    }
-  });
-};
-
-export default getConnection;
+export default pool;
