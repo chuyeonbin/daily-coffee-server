f16a32b4cc4a6d46ad279905d3ed869427d38b5d
ChuYeonBin <cndusqls98@gmail.com>
2023-07-12 16:25:04 +0900

    feat: 메일 보내기 route 구현

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import getConnection from '../database';
-import sendEmail, { transporter } from '../lib/mail';
+import { findUserByEmail, upsertEmailAuth } from '../database';
+import mailer from '../lib/mail';
+import { nanoid } from 'nanoid';
 
 const router = express.Router();
 
@@ -8,10 +9,24 @@ router.post('/sendemail', async (req, res) => {
   const email: string = req.body.email;
 
   try {
-    const info = await sendEmail(email);
+    const user = await findUserByEmail(email);
 
-    console.log(info);
-    transporter.close();
+    const code = nanoid(16);
+    await upsertEmailAuth(email, code);
+
+    try {
+      await mailer.sendEmail({
+        from: `"verify@daily-coffee.io" ${process.env.EMAIL_USER}`,
+        to: email,
+        subject: 'daily coffee 로그인',
+        text: '노드 패키지 nodemailer를 이용해 보낸 이메일임',
+        html: '<b>Hello world?</b>',
+      });
+    } catch (err) {
+      throw err;
+    } finally {
+      mailer.closeMail();
+    }
   } catch (err) {
     res.status(400).json('fail');
     throw err;
