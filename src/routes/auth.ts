d86204c487c1ac905d38a5d5a0bcb1c8bf354ebc
ChuYeonBin <cndusqls98@gmail.com>
2023-07-14 16:26:55 +0900

    feat: auth code router 구현

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,9 @@
 import express from 'express';
-import { findUserByEmail, upsertEmailAuth } from '../database';
+import {
+  findEmailAuthByCode,
+  findUserByEmail,
+  upsertEmailAuth,
+} from '../database';
 import mailer from '../lib/mail';
 import { nanoid } from 'nanoid';
 
@@ -20,7 +24,7 @@ router.post('/sendemail', async (req, res) => {
         to: email,
         subject: 'daily coffee 로그인',
         text: '노드 패키지 nodemailer를 이용해 보낸 이메일임',
-        html: '<b>Hello world?</b>',
+        html: `<b>Hello world?</b><br /><a href=""/>`,
       });
     } catch (err) {
       throw err;
@@ -28,11 +32,42 @@ router.post('/sendemail', async (req, res) => {
       mailer.closeMail();
     }
   } catch (err) {
-    res.status(400).json('fail');
+    res.status(400).send('fail');
     throw err;
   }
 
-  res.status(200).json('ok');
+  res.status(201).send('ok');
+});
+
+router.get('/code/:code', async (req, res) => {
+  const code = req.params.code;
+
+  try {
+    const emailAuth = await findEmailAuthByCode(code);
+
+    if (!emailAuth || emailAuth.logged) {
+      return res.status(201).json({ checked: false });
+    }
+
+    if (
+      (new Date().getTime() - emailAuth.updated_at.getTime()) / (60 * 1000) >
+      60
+    ) {
+      return res.status(201).json({ checked: false });
+    }
+
+    const user = await findUserByEmail(emailAuth.email);
+
+    if (!user) {
+      return res.status(201).json({ checked: true });
+    } else {
+      // 유저정보랑 jwt token 넘겨주기
+    }
+
+    res.status(500).json(null);
+  } catch (err) {
+    throw err;
+  }
 });
 
 export default router;
