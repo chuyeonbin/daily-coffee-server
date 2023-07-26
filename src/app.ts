19a18f5fd8faf31d6ef48faaa9a89154b77f70b1
ChuYeonBin <cndusqls98@gmail.com>
2023-07-26 17:14:37 +0900

    feat: passport init

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,29 @@
 import express from 'express';
-import sequelize from './db/models';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth';
+import userRouter from './routes/user';
+import passport from 'passport';
+import passportConfig from './passport/jwt';
 
 const app = express();
 
 app.use(
   cors({
     origin: true,
+    credentials: true,
   })
 );
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('데이터베이스 연결 성공');
-  })
-  .catch(console.error);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(passport.initialize());
+passportConfig();
 
 app.use('/auth', authRouter);
+app.use('/user', userRouter);
 
 app.listen('8080', () => {
   console.log(`
